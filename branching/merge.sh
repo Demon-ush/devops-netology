3eb2b6a3039b3f37999f021485594bef969f702c
Ekaterina <demon_ush@yahoo.com>
2021-05-06 22:33:01 +0300

    This is combination of 2 commits

--- branching/merge.sh
+++ branching/merge.sh
@@ -3,7 +3,14 @@
 # display command line options
 
 count=1
+<<<<<<< HEAD
 for param in "$*"; do
     echo "\$* Parameter #$count = $param"
     count=$(( $count + 1))
+=======
+
+for param in "$*"; do
+    echo "\$* Parameter #$count = $param"
+    count=$(( $count + 1 ))
+>>>>>>> 98a77ec... git-rebase 1
 done
--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,7 +3,18 @@
 # display command line options
 
 count=1
+<<<<<<< HEAD
 for param in "$*"; do
     echo "\$* Parameter #$count = $param"
     count=$(( $count + 1))
 done
+=======
+
+for param in "$#"; do
+    echo "\$* Parameter: $param"
+    count=$(( $count + 1 ))
+done
+
+echo "======="
+
+>>>>>>> 98a77ec... git-rebase 1
